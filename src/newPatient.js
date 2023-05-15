676028460dcf95226b552e0a213262b3166e6cb1
JFT1944 <96144063+JFT1944@users.noreply.github.com>
2023-05-15 13:11:24 -0400

    pinned background

--- src/newPatient.js
+++ src/newPatient.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function NewPatient(){
 
-
+function formSubmit(e){
+e.preventDefault(e)
+console.log(e.target.form)
+for (let i of e.target.form){
+    // console.log(i.placeholder)
+    // console.log(i.value)
+    console.log(`${i.placeholder}: ${i.value}`)
+}
+}
 
 
 
@@ -10,10 +18,10 @@ function NewPatient(){
         <>
         <div className="form-holder-holder">
             <div className="form-holder doctor_info">
-            <h2>About This Patient</h2>
         <form>
+            <h2>Tell Us About Yourself</h2>
             
-            <input type={'text'} placeholder='First Name' required/> 
+            <input type={'text'} placeholder='First Name' name='fName' required/> 
             <input type={'text'} placeholder='Last Name' required/> 
             <input type={'text'} placeholder='Street Address' required/> 
             <input type={'text'} placeholder='City' required/> 
@@ -22,8 +30,13 @@ function NewPatient(){
             <input type={'text'} placeholder='Phone' required/> 
             <input type={'text'} placeholder='Birthday' required/> 
             <input type={'text'} placeholder='Gender' required/> 
-            <input type={'text'} placeholder='Email' required/> 
-            <input style={{background:'rgba(145, 199, 147, 50%)'}} type={'submit'}/>
+            <input type={'email'} placeholder='Email' required/> 
+            <br />
+            <br />
+            <br />
+            <h2>Reason For This Visit</h2>
+            <input type={'text'} placeholder='Is this pain to:' required/> 
+            <input style={{background:'rgba(145, 199, 147, 50%)'}} type={'submit'} onClick={(e) => formSubmit(e)}/>
         </form>
             </div>
         </div>
