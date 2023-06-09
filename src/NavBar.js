da0533e507b56f2378ac5d34f258d8cee7f91753
JFT1944 <96144063+JFT1944@users.noreply.github.com>
2023-06-09 10:46:50 -0400

    new deal

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,6 +7,11 @@ import {NavLink} from 'react-router-dom'
 
 function NavBar(){
 let [dropdown, setDropDown] = useState(true)
+let [images, setImages] = useState({
+    logo:'https://lh6.googleusercontent.com/F7HPmjg5cDgG2Bt_zxbrt74EWBR3m14B1p83xDeTTh-CW6K-5tEyfSuuGv5iIe25LLeU9tY8Z0b1Cyt7u0NKH_gawcraLGSStSZ_-UhDCVN5sQYPPsXfn4b0b3S6YzPUul1o4z8tC0c3frSaHFMndyk', 
+    allDay:'https://www.cobbchiropracticclinic.com/images/Cobb_Chiropractic_Clinic_February2018/Images/chiropractor%20Greensboro.png?width=300&name=chiropractor%20Greensboro.png',
+
+})
 
 
 console.log('navbar')
@@ -31,6 +36,15 @@ return(
 
 
 <div className="nav_holder" style={{height:''}}>
+<div className="inner_nav_holder">
+    <div className="logo_holder">
+        <img src={images.logo} alt="Pro Healthcare Rehab Logo" className="logo"/>
+        <img src={images.allDay} alt="24/7 availablility" className="allday"/>
+        <span className="phone_num">{`(770) 800-2070 `}</span>
+        <span className="phone_num two"><span style={{color:'red'}}>Call Now!</span> We're waiting to answer your questions.</span>
+
+    </div>
+</div>
 
 <div className="nav">
     <NavLink exact to='/'>Home</NavLink>
