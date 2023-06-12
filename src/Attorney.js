40de6cbf80449b665ec887000abc4dce241a060e
JFT1944 <96144063+JFT1944@users.noreply.github.com>
2023-06-12 19:12:16 -0400

    Adding Content

--- src/Attorney.js
+++ src/Attorney.js
@@ -1,5 +1,18 @@
 import React from "react";
 import Banner1 from "./Banner1"
+import InfoSection from "./InfoSection";
+
+
+let data=[
+    {title1:`After a car accident you are going to get a call from the at fault party’s insurance company. Will you know what to say?`, text:[`You are going to need a personal injury attorney who will protect your interests and is skilled in auto accident cases.
+    The insurance company of the at fault party will try and offer you a minimal settlement after they send one of their representatives to take pictures of the crash.
+    Most people end up taking their initial offer because they don't know any better.
+    But in almost all cases they won’t offer you everything you deserve. They represent their company’s interests and not yours.
+    We can help you get the compensation you deserve, CALL PRO HEALTHCARE NOW! `]},
+    {title1:`Need help finding a good, qualified attorney?`, text:[`Our medical staff can refer you to some great local attorneys in the greater Marietta area that specialize in personal injury cases. The attorneys we refer you to don't charge huge fees. That means more money in your pocket from your settlement!`, `We work closely with your lawyer to provide them professional documentation of your injuries, diagnostic testing and treatment plan. Only with that medical documentation can your attorney get you the money you deserve for your case.`]},
+]
+
+
 
 
 function Attorney(){
@@ -10,6 +23,7 @@ function Attorney(){
     return(
     <>
     <Banner1 />
+    <InfoSection info={data}/>
     
     </>)
 }
--- src/AutoAccidents.js
+++ src/AutoAccidents.js
@@ -1,5 +1,14 @@
 import React from "react";
 import Banner1 from "./Banner1"
+import InfoSection from "./InfoSection";
+
+let data = [
+    {title1:`We Focus On Treating Car Accident Victims`, text:[`At Cobb Chiropractic we have been focusing on treating Car Accident Injury victims in Greensboro for more than 40 years.`, `Our Doctors understand that getting into a car accident  is a traumatic and stressful experience.
+    It can create an impact emotionally, mentally and physically.`, `How much more if the person who got caught in that accident suffers an injury? Even minor injuries can cause stress, pain, and inconvenience.`]},
+    {title1:`Treatment Options for Auto Accidents`, text:[`Our Chiropractors are experts in treating various injuries including neck injuries, back injuries as well as soft tissue injuries associated with automobile accidents.`, `In addition, they are also experts in loosening stiff muscles that were affected by the accident. Not only are we experienced in treating the injuries associated with Car Accidents, but we take care to provide the proper documentation of injuries, detailed narratives and the daily reports the Insurance companies and Attorneys need for the proper resolution of your Claim.`]},
+]
+
+
 
 
 function AutoAccidents(){
@@ -10,6 +19,7 @@ function AutoAccidents(){
     return(
     <>
     <Banner1 />
+    <InfoSection info={data}/>
     
     </>)
 }
