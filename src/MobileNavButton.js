8dcde2005cb8feea6aaf35e44957311542699675
JFT1944 <96144063+JFT1944@users.noreply.github.com>
2023-06-20 14:02:49 -0400

    touches to m_menu

--- src/MobileNavButton.js
+++ src/MobileNavButton.js
@@ -26,7 +26,14 @@ function handleClick(e){
             <span className="line top"></span>
             <span className="line middle"></span>
             <span className="line bottom"></span>
-            
+            <NavLink exact to='/'>Home</NavLink>
+            <NavLink exact to='/about'>About Us</NavLink>
+            <NavLink exact to='/injuries'>Injuries</NavLink>
+            <NavLink exact to='/chiropractic'>Chiropractic</NavLink>
+            <NavLink exact to='/auto-accidents'>Auto Accidents Care</NavLink>
+            <NavLink exact to='/medical'>Medical</NavLink>
+            <NavLink exact to='/attorney'>Attorney Referral</NavLink>
+            <NavLink exact to='/contact-us'>Contact Us</NavLink>
         </div>
         </>
     )
